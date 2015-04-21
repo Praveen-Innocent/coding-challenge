e0c929dc0baff5a9466cf51be25e05cf474b7a39
PC <pravin.innocent@gmail.com>
2015-04-22 05:07:28 +0530

    created the creatfile class

---
+++ createFile.php
@@ -0,0 +1,37 @@
+<?php
+
+class createFile {
+
+	//Config
+
+	//Different Object Types
+	private $alphabets ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	private $integers ="0123456789";
+	private $alpha_numeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	private $delimiter = ",";
+
+	private $items; 
+
+	public function __construct()
+	{
+			
+		
+	}
+
+	
+	public function randomString(){
+			
+	}
+
+	public function generateSequence() {
+
+	}
+
+	public function writeToFile() {
+	
+	}
+
+
+}
+
+?>
